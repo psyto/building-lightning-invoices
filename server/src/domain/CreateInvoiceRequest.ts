e457115f911ccbb9882464d2163da24fc2fa3c7c
bmancini55 <bmancini@gmail.com>
2022-06-03 14:58:10 -0400

    create CreateInvoiceRequest type

---
+++ server/src/domain/CreateInvoiceRequest.ts
@@ -0,0 +1,3 @@
+export interface CreateInvoiceRequest {
+    signature: string;
+}
---
+++ server/src/domain/IsCreateInvoiceRequest.ts
@@ -0,0 +1,5 @@
+import { CreateInvoiceRequest } from "./CreateInvoiceRequest";
+
+export function isCreateInvoiceRequest(val: unknown): val is CreateInvoiceRequest {
+    return typeof val === "object" && typeof val["signature"] === "string";
+}
