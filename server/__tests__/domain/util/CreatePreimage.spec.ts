9a19c7627ca17f858a33e311bc472ed725a3ba3c
bmancini55 <bmancini@gmail.com>
2022-06-09 16:23:22 -0400

    fix preimage tests

--- server/__tests__/domain/util/CreatePreimage.spec.ts
+++ server/__tests__/domain/util/CreatePreimage.spec.ts
@@ -2,18 +2,20 @@ import { expect } from "chai";
 import { createPreimage } from "../../../src/domain/util/CreatePreimage";
 
 describe("function: createPreimage", () => {
-    it("returns sha256(previous || signature)", () => {
+    it("returns sha256(local || remote || sats)", () => {
         // arrange
-        const previous = "0000000000000000000000000000000000000000000000000000000000000000";
-        const signature =
-            "rnqyje9tkhhmeaq1yjuok7nqkqumyhduxwijjahqaff718o8q45b6in883uit5dxoq7z59r8bhso99nwinbjo6efb8coro734jebete6";
+        const local =
+            "rypj5rexme7cqdqxzok1ygqw89h3m4qsu3dkje1xt894kmwwjr181sz5tnyz6o588bmx384sdx73ojnz3ebrnifxy67ykjfsfctjfns1";
+        const remote =
+            "rna7paf9c4ha5cjxwu849px7riqpdn6hd5gts8q9r4kb9sje64w4assrbithnjjipfuq3quceyp8b6rm9ifc9ddqan4ntej7yoj74snd";
+        const sats = 1000;
 
         // act
-        const result = createPreimage(previous, signature, 1);
+        const result = createPreimage(local, remote, sats);
 
         // assert
         expect(result.toString("hex")).to.equal(
-            "f9e15e200f506f2519fd5fbd3b64bb2add4adab97897f01022b86a5710166ecb",
+            "821c43d357903adbf257bb883e0441d8c095c0cbc595c6fcccca49f94378c860",
         );
     });
 });
