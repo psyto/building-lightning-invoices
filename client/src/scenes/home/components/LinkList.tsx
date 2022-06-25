9493feb5002637326fec6ba8946432938c9ce616
bmancini55 <bmancini@gmail.com>
2022-06-25 10:22:43 -0400

    client: hide list on first attempt

--- client/src/scenes/home/components/LinkList.tsx
+++ client/src/scenes/home/components/LinkList.tsx
@@ -3,6 +3,7 @@ import { Link as LinkRecord } from "../../../services/ApiTypes";
 import { Link } from "./Link";
 
 export const LinkList = ({ links }: { links: LinkRecord[] }) => {
+    if (links.length === 1) return <div></div>;
     return (
         <div style={{ borderTop: "1px solid #c0c0c0" }}>
             <h4 className="mt-3 mb-3">Ruler History</h4>
